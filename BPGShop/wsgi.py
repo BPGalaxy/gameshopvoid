b40df02e37a4ba05c5916a4d5b4ca6002db9f6aa
Arthur <100694474+BPGalaxy@users.noreply.github.com>
2026-05-30 20:22:08 +0330

    Add files via upload

---
+++ BPGShop/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for BPGShop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+path = 'BPGShop/BPGShop'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'BPGShop.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
--- home/models.py
+++ home/models.py
@@ -17,6 +17,7 @@ class Game(models.Model):
     attrs = models.JSONField(default=list)
     price = models.IntegerField(default=0)
     image = models.ImageField(default="")
+    download_link = models.TextField(default="")
     def __str__(self):
         return self.name
 
